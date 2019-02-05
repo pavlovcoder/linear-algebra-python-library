274d0b165a3473e5b827f00b2ec784e94352148e
Vladimir Pavlov <w.pavlov@hit-kody.com.pl>
2019-02-05 22:01:07 +0100

    Commit #3

--- vectors_initialize.py
+++ vectors_initialize.py
@@ -17,7 +17,7 @@ print(
 
 #Default function for handling execution loop:
 def execution_loop():
-  data = int(input("Do you want to try again ? Enter [1] - for continue / [0] - for quit : "))
+  data = int(input("Do you want to try again ? Enter [1] - for continue / [0] - for quit :\n>>>"))
   if data == 1:
     return True
   elif data == 0:
@@ -27,7 +27,7 @@ def execution_loop():
     execution_loop()
 
 def vector_loop():
-  data = int(input("Do you want to add a new coordinate ? Enter:\n[2] - for adding a new coordinate;\n[1] - for adding a new vector;\n[0] - for open operations panel"))
+  data = int(input("Do you want to add a new coordinate ? Enter:\n[2] - for adding a new coordinate;\n[1] - for adding a new vector;\n[0] - for open operations panel\n>>>"))
   if data == 2:
     return 2
   elif data == 1:
@@ -39,13 +39,14 @@ def vector_loop():
     vector_loop()
 
 def vector_loop_max():
-  data = int(input("Do you want to add a new vector ? Enter:\n[1] - for adding a new vector;\n[0] - for open operations panel"))
+  data = int(input("Do you want to add a new vector ? Enter:\n[1] - for adding a new vector;\n[0] - for open operations panel\n>>>"))
   if data == 1:
     return 1
   elif data == 0:
     return 0
   else:
     print("Error: you entered incorrect command. Please, try again...")
+    vector_loop_max()
 
 #Module for creating a simple mathematical vectors on the dimension:
 class Vector(object):
@@ -75,42 +76,45 @@ def vectors_loop_creating():
     coordinate_counter = 1
     vector_input_control = True
     while vector_input_control:
-      print("Please, enter all coordinates of the %d vector:" % vector_counter)
+      #print("Please, enter all coordinates of the %d vector:" % vector_counter)
+      print(f'Please, enter all coordinates of the {vector_counter}-vector:')
       vector = []
       coordinate_counter = 1
       coordinate_input_control = True
       while coordinate_input_control:
-        if len(vector) != 3:
-          print("Please, enter %d-st coordinate:" % coordinate_counter)
-          vector.append(int(input("")))
-          coordinate_input_control = vector_loop()
-          data = vector_loop()
-          if data == 2:
-            coordinate_input_control = True
-          elif data == 1:
-            coordinate_input_control = False
-            break
-          else:
-            coordinate_input_control = False
-            vector_input_control = False
-            break
-          coordinate_counter = coordinate_counter + 1
-        else:
-          print("You almost entered the maximum number of coordinates on the %d-vector." % vector_counter)
+        #print("Please, enter %d-st coordinate:" % coordinate_counter)
+        print(f'Please, enter {coordinate_counter}-st coordinate:')
+        vector.append(int(input(">>>")))
+        if len(vector) == 3:
+          #print("You almost entered the maximum number of coordinates on the %d-vector." % vector_counter)
+          print(f'You almost entered the maximum number of coordinates on the {vector_counter}-vector. The maximum number of coordinates should be less of equal to 3.')
           coordinate_input_control = False
           data = vector_loop_max()
           if data == 1:
             vector_input_control = True
           else:
             vector_input_control = False
+          break
+        data = vector_loop()
+        if data == 2:
+          coordinate_input_control = True
+        elif data == 1:
+          coordinate_input_control = False
+          break
+        else:
+          coordinate_input_control = False
+          vector_input_control = False
+          break
+        coordinate_counter = coordinate_counter + 1
+      vector_counter = vector_counter + 1
       print("You entered vector with coordinates: ", vector)
-      vectors.append(vector)
-    print("You entered vectors-list:")
-    print(vectors)
+      vectors.append(Vector(vector))
+    return vectors
 
 #Function for handling all operations on the vectors:
-def vectors_operations_handling():
-  print('You called panel operations for entered vectors-list... Coming soon...')
+def vectors_operations_handling(vectors):
+  print('Output 1-th vector:')
+  print(vectors[0])
 
 #Default parameters for handling execution loop:
 again_exec = True
@@ -120,7 +124,7 @@ counter_exec = 0
 while again_exec:
   vectors = []
   vectors = vectors_loop_creating()
-  vectors_operations_handling()
+  vectors_operations_handling(vectors)
   again_exec = execution_loop()
   counter_exec = counter_exec + 1
 
